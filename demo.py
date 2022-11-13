bd359989df164849aeb01873c844bcc8095309ba
Ashis Das <105511844+ashis-das@users.noreply.github.com>
2022-11-13 20:19:03 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,6 @@
+#  Find Volume of cylinder
+#take two user input number
+radius=float(input("enter the radius of the cylinder"))
+height=float(input("enter the hight of the cylinder"))
+volume=(3.14)*(radius**2)*(height) #as the value of pi is 3.14
+print("the volume of the cylinder is", volume)
